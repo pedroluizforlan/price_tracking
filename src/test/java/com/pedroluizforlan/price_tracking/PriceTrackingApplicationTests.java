218e6a2df761e9c95d3568b9a3b9c7f03a06bb89
Pedro Luiz Forlan de Paula <pedro.forlan2000@gmail.com>
2025-05-16 23:37:51 -0300

    Starting Project

---
+++ src/test/java/com/pedroluizforlan/price_tracking/PriceTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroluizforlan.price_tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceTrackingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
